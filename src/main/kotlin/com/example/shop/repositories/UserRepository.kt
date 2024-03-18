b60f350e5aa525d59dc336ee3f177fb285caa766
polinasav <postscriptum145@gmail.com>
2024-03-18 13:58:07 +0300

    added Dockerfile

--- src/main/kotlin/com/example/shop/repositories/UserRepository.kt
+++ src/main/kotlin/com/example/shop/repositories/UserRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
-@Repository
+
 interface UserRepository: CrudRepository<User, Long> {
     fun findByEmail(email: String): User?
 
