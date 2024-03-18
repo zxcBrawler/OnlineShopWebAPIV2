c10513ac7efec53b367a9364beba5dc23eb79fb3
polinasav <postscriptum145@gmail.com>
2024-03-18 13:30:34 +0300

    added Dockerfile

---
+++ src/main/kotlin/com/example/shop/config/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.shop.config
+
+
+ object Constants{
+     val SECRET_KEY = "d9c51ce08d288d55aaa28dbfcfd6c7270ce4ebc35d05aa22dfa1a1369983c745"
+}
---
+++ src/main/kotlin/com/example/shop/models/dto/ShopGarnishDTO.kt
@@ -0,0 +1,6 @@
+package com.example.shop.models.dto
+
+class ShopGarnishDTO{
+    var shopGarnishId = 0
+    var quantity = 0
+}
