6da9758c0940f009f0348a2d4114511772b28d55
polinasav <postscriptum145@gmail.com>
2024-03-23 17:00:37 +0300

    minor fixes

--- src/main/kotlin/com/example/shop/controllers/OrderCompositionController.kt
+++ src/main/kotlin/com/example/shop/controllers/OrderCompositionController.kt
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/api/ordersComp")
 class OrderCompositionController (@Autowired private val orderCompositionRepository: OrderCompositionRepository)  {
